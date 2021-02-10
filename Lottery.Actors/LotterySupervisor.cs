0adf703737ada33ed3d87661208f4d3cca2ebf5b
Andy Orton <andyorton13@gmail.com>
2021-02-10 12:06:52 -0700

    project infrastructure in place

---
+++ Lottery.Actors/LotterySupervisor.cs
@@ -0,0 +1,22 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lottery.Actors
+{
+    public class LotterySupervisor : UntypedActor
+    {
+        public ILoggingAdapter Log { get; } = Context.GetLogger();
+
+        protected override void PreStart() => Log.Info("Lottery Application started");
+        protected override void PostStop() => Log.Info("Lottery Application stopped");
+
+        protected override void OnReceive(object message)
+        {
+        }
+
+        public static Props Props() => Akka.Actor.Props.Create<LotterySupervisor>();
+    }
+}
---
+++ Lottery.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Actor;
+using Lottery.Actors;
+using System;
+
+namespace Lottery.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Init();
+        }
+
+        public static void Init()
+        {
+            using (var system = ActorSystem.Create("lottery-system"))
+            {
+                // Create top level supervisor
+                var supervisor = system.ActorOf(LotterySupervisor.Props(), "lottery-supervisor");
+                // Exit the system after ENTER is pressed
+                Console.ReadLine();
+            }
+        }
+    }
+}
